497b7541cd7372c852719fb4757aec46adf23709
Nick Strayer <nick.strayer@gmail.com>
2018-09-03 22:48:59 +0000

    Added some basic snp info and and cleaned up the axis format for the maf plot.

--- d3_plots/info_banner.js
+++ d3_plots/info_banner.js
@@ -1,39 +1,60 @@
 // Info banner for SNP name, MAF for current selection and the entire exome cohort. Along with link to click for genome browser location of SNP.
+const {snp, maf_exome, maf_sel, gene, chromosome} = data;
+
+const padding = 10;
 const exome_color = 'steelblue';
 const sel_color = 'orangered';
 const maf_chart_start = width/3;
 const label_gap= 35;
 const point_r = 15;
+const max_freq = Math.max(maf_exome, maf_sel)*1.1;
 
-const {snp, maf_exome, maf_sel} = data;
+// turns proportion into a rounded percentange
+const toPercent = d3.format(".1%");
 
 // draw the snp name in the upper left corner
 svg.append('text')
   .text(snp)
   .attr('class', 'snp_name')
   .attr('x', 10)
-  .attr('y', 25);
+  .attr('y', 10);
+  
+svg.append('text')
+  .html(`Gene: <tspan class = 'snp_info'>${gene}</tspan>`)
+  .attr('x', 10)
+  .attr('y', height - 50);
+  
+svg.append('text')
+  .html(`Chromosome: <tspan class = 'snp_info'>${chromosome}</tspan>`)
+  .attr('x', 10)
+  .attr('y', height - 30);
+  
+svg.append('text')
+  .html(`Genome Browser Link`)
+  .attr('class', 'genome_browser_link')
+  .attr('x', 10)
+  .attr('y', height - 10);
   
 // MAF scale
 const x = d3.scaleLinear()
-  .domain([0,Math.max(maf_exome, maf_sel)*1.1])
-  .range([maf_chart_start, width]);
+  .domain([0,max_freq])
+  .range([maf_chart_start, width - padding]);
   
-  
-svg.append('line')
-  .attr('class', 'axis_line')
-  .attr('x1', maf_chart_start)
-  .attr('x2', width)
-  .attr('y1', height/2)
-  .attr('y2', height/2);
 
+svg.append("g")
+  .attr("transform", `translate(0,${height/2})`)
+  .call( d3.axisBottom(x)
+    .tickValues([0, max_freq])
+    .tickFormat(toPercent)
+    .tickSizeOuter(0)
+  );
+      
   
 const maf_plot = svg.selectAll('#maf_plot')
   .data([
     {group: 'Exome Cohort', maf: maf_exome},
     {group: 'Current Selection', maf: maf_sel}
   ]);
-  
 
 // lines from axis to group label
 maf_plot.enter().append('line')
@@ -61,11 +82,10 @@ maf_plot.enter().append('circle')
   
 
 maf_plot.enter().append('text')
-  .text(d => d.maf)
+  .text(d => toPercent(d.maf))
   .attr('class', 'maf_points')
   .attr('x', d => x(d.maf))
   .attr('y', d => height/2)
   .attr('font-size', 15);
-
   
 svg.style('background', 'lightblue');
---
+++ d3_plots/runner_scripts/run_info_banner.R
@@ -0,0 +1,16 @@
+# script to run the info banner r2d3 with simulated data.
+
+snp <- 'rs13283456'
+
+results <- getSNPInfo(snp)
+
+results$snp <- snp
+results$maf_exome <- 0.05
+results$maf_sel <- 0.06
+
+r2d3::r2d3(
+  results, 
+  script = here('d3_plots/info_banner.js'), 
+  css = here('d3_plots/info_banner.css'), 
+  height = '150px'
+)
