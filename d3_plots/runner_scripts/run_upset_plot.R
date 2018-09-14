ba1e68625bce8a1b87407c61513e5067ce762ea1
Nick Strayer <nick.strayer@gmail.com>
2018-09-14 15:54:17 +0000

    Tiny changes with the runner script for the upset javascript.

--- d3_plots/runner_scripts/run_upset_plot.R
+++ d3_plots/runner_scripts/run_upset_plot.R
@@ -8,10 +8,10 @@ optionsData <- here('data/upset_data/optionsData.rds') %>% read_rds()
 setData <- here('data/upset_data/setData_all.rds') %>% read_rds()
 optionsData <- here('data/upset_data/optionsData_all.rds') %>% read_rds()
 
-setData %>% 
-  r2d3(
-    script = here('d3_plots/upset_interactive.js'), 
-    css = here('d3_plots/upset.css'),
-    dependencies = "d3-jetpack",
-    options = list(marginalData = marginalData, overallMaRate = overallMaRate)
-  )
+r2d3(
+  data = setData,
+  script = here('d3_plots/upset_interactive.js'), 
+  css = here('d3_plots/upset.css'),
+  dependencies = "d3-jetpack",
+  options = optionsData
+)
